46be411a0ad765cd1f373156acb7497508fbd0ee
jose51igual <avila51jose@gmail.com>
2025-02-21 14:03:40 +0100

    [MOD] Update message origin check in AuthRepository to use production URL

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -44,7 +44,7 @@ export default class AuthRepository {
     return new Promise((resolve) => {
       const messageHandler = (event) => {
         console.log('Received message:', event);
-        if (event.origin !== 'http://localhost') {
+        if (event.origin !== 'https://frontend.projectogb4.ddaw.es/') {
           console.warn(`Origin mismatch: expected ${origin}, got ${event.origin}`);
           return;
         }
